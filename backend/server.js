c56ee796cd78102eb859cb7e793c7bda833eeb37
glc300 <cwanjau52@gmail.com>
2024-10-16 22:43:23 +0300

    created yolo-client and yolo-backend,pushed images to docker hub and attached a screenshot to README

--- backend/server.js
+++ backend/server.js
@@ -7,7 +7,7 @@ const upload = multer();
 const productRoute = require('./routes/api/productRoute');
 
 // Connecting to the Database
-let mongodb_url = 'mongodb://localhost/';
+let mongodb_url = 'mongodb+srv://CW123:21nRZFP1fsbVQUc0@cwdemocluster.vwes6.mongodb.net/?retryWrites=true&w=majority&appName=CWdemocluster';
 let dbName = 'yolomy';
 
 // define a url to connect to the database
